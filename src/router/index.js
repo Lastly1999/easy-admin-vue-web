73b7aa2e6d549e6213edb6312a7aef9a62033da5
Lastly1999 <Lastly1999@163.com>
2022-05-25 17:19:39 +0800

    web app router edit commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+console.log(process.env)
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+/**
+ * 路由自动化注册
+ */
+const requireComponent = require.context('@/views', true, /index.vue$/) // 找到views路径下的所有文件
+const dynamicRoute = requireComponent.keys().filter(fileName => {
+  if (fileName === './index.vue') { // 过滤掉父节点的路由
+    return false
+  } else {
+    return true
   }
-]
+})
+  .map(fileName => {
+    const componentConfig = requireComponent(fileName)
+    const componentName = fileName.replace(/^\.\//, '').replace(/\.vue$/, '')// 剥去文件名开头的 `./` 和`.vue`结尾的扩展名
+    const componentNameRe = componentName.replace(/\//g, '-') // 设置name为文件夹名-index
+    const component = Vue.component(componentNameRe, componentConfig.default || componentConfig) // 根据路径注册成组件
+    const result = {
+      path: `/${componentName.toLowerCase()}`,
+      name: componentNameRe,
+      component
+    }
+    return result
+  })
 
 const router = new VueRouter({
-  routes
+  dynamicRoute
 })
 
 export default router
