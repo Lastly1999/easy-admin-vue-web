97c9dc9313f1830ccd85d6e2d17ae72217858075
unknown <605944384@qq.com>
2022-05-30 21:20:27 +0800

    登录页开发

---
+++ src/views/Base/Home/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-row class="container">
+      <el-col :span="24" class="header">
+        <el-col :span="18" class="logo">
+          <img src="@/assets/logo.png" alt="">
+        </el-col>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
+.header {
+  height: 60px;
+  line-height: 60px;
+  background: skyblue;
+}
+img{
+  width: 200px;
+}
+</style>
--- src/views/Base/Login/index.vue
+++ src/views/Base/Login/index.vue
@@ -1,54 +1,16 @@
 <template>
   <div class="container">
-    <el-row>
-      <el-col :span="24"
-        ><div class="grid-content bg-purple-dark logo">
-          <img src="../../../assets/logo.png" alt="" /></div
-      ></el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="24"
-        ><div class="grid-content bg-purple-dark logo">
-          <div><el-input
-              v-model="admin"
-              placeholder="请输入账号"
-              class="incontainer"
-              prefix-icon="el-icon-user-solid"
-            ></el-input>
-          </div></div
-      ></el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="24"
-        ><div class="grid-content bg-purple-dark logo">
-          <div>
-            <el-input
-              type="password"
-              v-model="passWord"
-              placeholder="请输入密码"
-              class="incontainer"
-              prefix-icon="el-icon-goods"
-            ></el-input>
-          </div></div
-      ></el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="24"
-        ><div class="grid-content bg-purple-dark logo">
-          <el-button type="primary" class="incontainer">登录</el-button>
-        </div></el-col
-      >
-    </el-row>
-    <div class="demo-image">
-  <div class="block" v-for="fit in fits" :key="fit">
-    <span class="demonstration">{{ fit }}</span>
-    <el-image
-      style="width: 100px; height: 100px"
-      src="../../../assets/Work.jepg"
-      :fit="fit"></el-image>
-  </div>
-</div>
-
+    <el-container>
+      <el-aside width="520px">
+        <div>
+          <img src="@/assets/logo.png" alt="" width="120px">
+        </div>
+        
+      </el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -59,29 +21,32 @@ export default {
     return {
       admin: "",
       passWord: "",
-      style: {},
     };
+  },
+  methods: {
+    showHome() {
+      this.$router.push({ path: "/base/home/index" });
+    },
   },
 };
 </script>
 
 <style scoped>
-.container {
-  /* border: 1px solid #000; */
-  width: 400px;
-  height: 600px;
-  margin: 0 auto;
-  padding-top: 60px;
+
+.el-aside {
+  background-color: #f1cdd7;
+  color: #333;
+  text-align: center;
+  /* box-sizing: content-box; */
+  height: 100vh;
+  padding:50px ;
 }
-.logo {
-  display: flex;
-  justify-content: space-around;
+
+.el-main {
+  
+  color: #333;
+  text-align: center;
+  /* line-height: 160px; */
 }
-.logo > img {
-  width: 150px;
-}
-.incontainer {
-  width: 300px;
-  margin-top: 20px;
-}
+
 </style>
